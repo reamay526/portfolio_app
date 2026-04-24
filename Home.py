d771e3c5b6171a32dd802c936ee8718e8d736fe7
Rea May Villanueva <reavillanueva76@gmail.com>
2026-04-24 18:09:18 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,18 +2,13 @@ import streamlit as st
 
 st.set_page_config(page_title="My Portfolio", page_icon="🌐")
 
+st.markdown("""
+<style>
+.stApp { background-color: #f7f3ff; }
+</style>
+""", unsafe_allow_html=True)
+
 st.title("Welcome to My Portfolio")
-
-st.write("""
-Navigate through my portfolio using the sidebar.
-
-Sections included:
-- Home
-- About Me
-- Skills
-- Projects
-- Contact
-""")
 
 col1, col2 = st.columns(2)
 
