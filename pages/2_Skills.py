95d6bac2724c97f65e6a5efac64f391977c06c77
Rea May Villanueva <reavillanueva76@gmail.com>
2026-04-24 17:40:32 +0800

    Add CSS skill slider and custom background style

--- pages/2_Skills.py
+++ pages/2_Skills.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background: linear-gradient(to right, #74ebd5, #ACB6E5);
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.title("Skills")
 
 python = st.slider("Python", 0, 100, 70)
@@ -8,5 +19,9 @@ st.progress(python)
 html = st.slider("HTML", 0, 100, 60)
 st.progress(html)
 
+css = st.slider("CSS", 0, 100, 55)
+st.progress(css)
+
 st.checkbox("GitHub")
 st.checkbox("Streamlit")
+st.checkbox("VS Code")
