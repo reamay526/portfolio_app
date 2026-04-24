42cff28556e333f2a951dbaf77489dae5c2e00f1
Rea May Villanueva <reavillanueva76@gmail.com>
2026-04-24 21:16:25 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,26 +19,20 @@ html, body, .stApp {
 
 h1, h2, h3 {
     color: #000000 !important;
-    text-align: center;
 }
 </style>
 """, unsafe_allow_html=True)
 
-col1, col2, col3 = st.columns([1, 2, 1])
+col1, col2 = st.columns([1, 2])
+
+with col1:
+    st.image("mayie.png", width=220)
 
 with col2:
-    left, right = st.columns([1, 2])
+    st.title("Welcome to My Portfolio")
+    st.write("Rea May M. Villanueva")
+    st.write("BSCS 3B")
+    st.write("Aspiring Developer & Designer")
+    st.write("Interested in Web Development")
 
-    with left:
-        st.image("mayie.png", width=200)
-
-    with right:
-        st.markdown("<div style='text-align:center;'>", unsafe_allow_html=True)
-        st.title("Welcome to My Portfolio")
-        st.write("Rea May M. Villanueva")
-        st.write("BSCS 3B")
-        st.write("Aspiring Developer & Designer")
-        st.write("Interested in Web Development")
-
-        st.button("Say Hello 👋")
-        st.markdown("</div>", unsafe_allow_html=True)
+    st.button("Say Hello 👋")
