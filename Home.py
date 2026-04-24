3bf4af8ebadf1f94a6be12cef358d7dfdda83833
Rea May Villanueva <reavillanueva76@gmail.com>
2026-04-24 18:23:11 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,20 +7,24 @@ st.markdown(
     <style>
     .stApp {
         background: linear-gradient(135deg, #3b1b5a, #e6e6e6, #ff4fa3);
+        text-align: center;
     }
 
     html, body, .stApp {
         color: #000000 !important;
+        text-align: center;
     }
 
     h1, h2, h3, h4, h5, h6,
     p, span, label, div {
         color: #000000 !important;
+        text-align: center;
     }
 
     input, textarea {
         background-color: rgba(255,255,255,0.9) !important;
         color: #000000 !important;
+        text-align: center;
     }
 
     section[data-testid="stSidebar"] {
