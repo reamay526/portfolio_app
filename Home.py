dc447afbdef7139d1ef7c297f417be446eca3ce1
Rea May Villanueva <reavillanueva76@gmail.com>
2026-04-24 21:06:21 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -23,19 +23,16 @@ h1, h2, h3 {
 </style>
 """, unsafe_allow_html=True)
 
-col1, col2, col3 = st.columns([1, 2, 1])
+col1, col2 = st.columns([1, 2])
+
+with col1:
+    st.image("mayie.png", width=220)
 
 with col2:
-    left, right = st.columns([1, 2])
+    st.title("Welcome to My Portfolio")
+    st.subheader("Rea May M. Villanueva")
+    st.write("BSCS 3B")
+    st.write("Aspiring Developer & Designer")
+    st.write("Interested in Web Development")
 
-    with left:
-        st.image("mayie.png", width=220)
-
-    with right:
-        st.title("Welcome to My Portfolio")
-        st.subheader("Rea May M. Villanueva")
-        st.write("BSCS 3B")
-        st.write("Aspiring Developer & Designer")
-        st.write("Interested in Web Development")
-
-        st.button("Say Hello 👋")
+    st.button("Say Hello 👋")
