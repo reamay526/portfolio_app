9b974f3638b9622de62f11178cb5182e87c639ba
Rea May Villanueva <reavillanueva76@gmail.com>
2026-04-24 21:07:52 +0800

    Update 3_Projects.py

--- pages/3_Projects.py
+++ pages/3_Projects.py
@@ -1,5 +1,22 @@
 import streamlit as st
 
+st.markdown("""
+<style>
+.stApp {
+    background: linear-gradient(135deg, #3b1b5a, #e6e6e6, #ff4fa3);
+    font-family: Arial;
+}
+
+html, body, .stApp {
+    color: #000000 !important;
+}
+
+.block-container {
+    padding-top: 3rem;
+}
+</style>
+""", unsafe_allow_html=True)
+
 st.title("Projects")
 
 st.subheader("Portfolio App")
