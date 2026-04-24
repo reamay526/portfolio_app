d890897a813358ec3c9a0edfb719fbc0885df1d4
Rea May Villanueva <reavillanueva76@gmail.com>
2026-04-24 21:12:58 +0800

    Update 1_About.py

--- pages/1_About.py
+++ pages/1_About.py
@@ -1,38 +1,30 @@
 import streamlit as st
 
-st.set_page_config(page_title="My Portfolio", layout="wide")
-
 st.markdown("""
 <style>
 .stApp {
     background: linear-gradient(135deg, #3b1b5a, #e6e6e6, #ff4fa3);
 }
 
-html, body, .stApp {
-    color: #000000 !important;
-    font-family: Arial;
-}
-
 .block-container {
     padding-top: 3rem;
 }
 
-h1, h2, h3 {
+html, body, .stApp {
     color: #000000 !important;
 }
 </style>
 """, unsafe_allow_html=True)
 
-col1, col2 = st.columns([1, 2])
+st.title("About Me")
 
-with col1:
-    st.image("mayie.png", width=220)
+st.markdown("""
+I enjoy learning programming and building simple applications.
+""")
 
-with col2:
-    st.title("Welcome to My Portfolio")
-    st.write("Rea May M. Villanueva")
-    st.write("BSCS 3B")
-    st.write("Aspiring Developer & Designer")
-    st.write("Interested in Web Development")
+st.subheader("Education")
+st.write("BS Computer Science")
 
-    st.button("Say Hello 👋")
+st.subheader("Goals")
+st.write("Improve coding skills")
+st.write("Build useful projects")
