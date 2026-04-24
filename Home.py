ce5f7919d50ec554b75a8c3220aca5c7757c4410
Rea May Villanueva <reavillanueva76@gmail.com>
2026-04-24 18:15:47 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="My Portfolio", page_icon="🌐")
 
 # STYLE
-st.markdown("""<style>.stApp{background: linear-gradient(135deg,#3b1b5a,#e6e6e6,#ff4fa3);}html,body,.stApp{color:#000 !important;}</style>""", unsafe_allow_html=True)
+st.markdown("""<style>.stApp{background:linear-gradient(135deg,#3b1b5a,#e6e6e6,#ff4fa3);}html,body,.stApp{color:#000 !important;}</style>""", unsafe_allow_html=True)
 
 st.title("Welcome to My Portfolio")
 
