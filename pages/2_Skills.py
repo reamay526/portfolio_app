a50750195cc1aeb642f5e5817e8899c7dc89a867
Rea May Villanueva <reavillanueva76@gmail.com>
2026-04-24 21:13:15 +0800

    Update 2_Skills.py

--- pages/2_Skills.py
+++ pages/2_Skills.py
@@ -4,15 +4,14 @@ st.markdown("""
 <style>
 .stApp {
     background: linear-gradient(135deg, #3b1b5a, #e6e6e6, #ff4fa3);
-    font-family: Arial;
-}
-
-html, body, .stApp {
-    color: #000000 !important;
 }
 
 .block-container {
     padding-top: 3rem;
+}
+
+html, body, .stApp {
+    color: #000000 !important;
 }
 </style>
 """, unsafe_allow_html=True)
