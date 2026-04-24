dfbc3c42975d9af1a574e0866bd3c3180edae9e7
Rea May Villanueva <reavillanueva76@gmail.com>
2026-04-24 17:44:18 +0800

    Update 4_Contact.py

--- pages/4_Contact.py
+++ pages/4_Contact.py
@@ -1,18 +1,31 @@
 import streamlit as st
 
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background: linear-gradient(to right, #74ebd5, #ACB6E5);
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.title("Contact Me")
 
 with st.form("contact_form"):
     name = st.text_input("Name")
     email = st.text_input("Email")
     message = st.text_area("Message")
+
     submit = st.form_submit_button("Send")
 
     if submit:
         if name and email and message:
             st.success("Message sent successfully! ✅")
         else:
-            st.error("Please fill all fields")
+            st.error("Please fill all fields.")
 
 st.markdown("### Social Links")
-st.write("- GitHub:  https://github.com/reamay526")
+st.write("- GitHub: https://github.com/reamay526")
+st.write("- Facebook: https://www.facebook.com/rea.villanueva.9277")
