2345a3fe3d7eef98262399a6bfdfe978683f7142
Rea May Villanueva <reavillanueva76@gmail.com>
2026-04-24 17:40:05 +0800

    Update 1_About.py

--- pages/1_About.py
+++ pages/1_About.py
@@ -1,11 +1,26 @@
 import streamlit as st
 
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background: linear-gradient(to right, #74ebd5, #ACB6E5);
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.title("About Me")
 
 st.write("""
-I am Rea May M. Villanueva, a BSCS student.
-I am learning programming and building simple applications.
+I am Rea May M. Villanueva, a BSCS student currently learning programming and web development.
 """)
 
 st.subheader("Education")
-st.write("BS Computer Science (BSCS 3B)")
+st.write("Bachelor of Science in Computer Science")
+
+st.subheader("Interest")
+st.write("- Programming")
+st.write("- Web Development")
+st.write("- UI Design")
